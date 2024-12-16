186cb39ef08fb65d86bbf291a1b9287616fca002
Atabekov Ibragim <edinstvenniy123@gmail.com>
2024-12-16 12:06:04 +0300

    fix: gitignore,lsf creation

--- create_lsf.py
+++ create_lsf.py
@@ -5,7 +5,7 @@ def generate_task(T, AffinityT, Nx, Ny):
         #BSUB -o res/{Nx}_{T}
         #BSUB -e err/a.out.%J.err
         #BSUB -R "affinity[core({AffinityT})]"
-        OMP_NUM_THREADS={AffinityT} /polusfs/lsf/openmp/launchOpenMP.py ./main {Nx} {Ny} 7 10 {T} 0.00001 no_debug
+        OMP_NUM_THREADS={T} /polusfs/lsf/openmp/launchOpenMP.py ./main {Nx} {Ny} 7 10 {T} 0.00001 no_debug
     """
     return task
 
